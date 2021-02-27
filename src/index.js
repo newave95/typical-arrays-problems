b466aaab8af4646c45fe26097137b63dda3db239
newave95 <derevianko.ads@gmail.com>
2021-02-27 08:41:29 +0100

    init: main functional was completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 1) {
+  if(array == 1){
+    return 0;
+  }else if(array.length == 0){
+    return 0;
+  }
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 1) {
+  if(array == 1){
+    return 0;
+  }else if(array.length == 0){
+    return 0;
+  }
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 1) {
+  if(array == 1){
+    return 0;
+  }else if(array.length == 0){
+    return 0;
+  }
+  let total = 0;
+  for(i of array){
+    total += i;
+  }
+  return total/array.length;
 }
